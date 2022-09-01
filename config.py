9656ceea9c6cdd520b27ec376dce8826f79c8114
Alexandr <102321532+h4rdhero@users.noreply.github.com>
2022-09-01 13:32:15 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '5603227197:AAFDdFElSHlhzsmsmV4Ll5KCRorou-J4EHo'
+#5603227197:AAFDdFElSHlhzsmsmV4Ll5KCRorou-J4EHo
+
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'биткоин': 'BTC',
+}
